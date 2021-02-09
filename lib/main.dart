557bc38d5698d648d1a4f9a04eddb0a63680ec33
Julio Canizalez <juliocanizalez@outlook.com>
2021-02-09 17:50:54 -0600

    :rocket: finish app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Julio Canizalez"),
+        backgroundColor: Colors.red[700],
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(image: AssetImage('assets/img/yo.jpeg')),
+          ],
+        ),
+      ),
+    ),
+  ));
 }
